5bc6b2979fcd6d6d4d219913d1e1a974329edff2
Шостка А.С <shostka@lanit.ru>
2022-07-03 21:25:59 +0300

    unnecessary config removed

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const app = require("./app");
-const { config } = require("dotenv").config();
 
 app.listen(process.env.PORT, () => {
   console.log(`Server has been started on ${process.env.PORT}`);
